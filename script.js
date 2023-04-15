13b7d93bec9ae3abc070128b662661a6a9258277
Sebastian <sebastiansloma@gmail.com>
2023-04-15 09:47:05 +0200

    added slide photo swiper js

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-
+// menu
 function toggleMenu(){
     const toggleMenu = document.querySelector('.toggleMenu')
     const navigation = document.querySelector('.navigation')
     toggleMenu.classList.toggle('active')
     navigation.classList.toggle('active')
 }
+// swipper
+var swiper = new Swiper(".live-swiper", {
+    slidesPerView: 1,
+    centeredSlides: false,
+    slidesPerGroupSkip: 1,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    breakpoints: {
+      769: {
+        slidesPerView: 2,
+        slidesPerGroup: 1,
+      },
+    },
+   
+    navigation: {
+      nextEl: ".live-swiper .swiper-button-next",
+      prevEl: ".live-swiper .swiper-button-prev",
+    },
+ 
+  });
 
