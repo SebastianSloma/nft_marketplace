87ad8643c27396a8ff71066c7d030cddaa05aaee
Sebastian <sebastiansloma@gmail.com>
2023-04-16 11:11:30 +0200

    created explore art section

--- script.js
+++ script.js
@@ -1,57 +1,62 @@
 // menu
-function toggleMenu(){
-    const toggleMenu = document.querySelector('.toggleMenu')
-    const navigation = document.querySelector('.navigation')
-    toggleMenu.classList.toggle('active')
-    navigation.classList.toggle('active')
+function toggleMenu() {
+	const toggleMenu = document.querySelector('.toggleMenu');
+	const navigation = document.querySelector('.navigation');
+	toggleMenu.classList.toggle('active');
+	navigation.classList.toggle('active');
 }
 // swipper
-const swiper = new Swiper(".live-swiper", {
-    slidesPerView: 3,
-   spaceBetwen:30,
-    slidesPerGroupSkip: 1,
-    grabCursor: true,
-    keyboard: {
-      enabled: true,
-    },
-    
-   
-    navigation: {
-      nextEl: ".live-swiper .swiper-button-next",
-      prevEl: ".live-swiper .swiper-button-prev",
-    },
-    breakpoints: {
-      "@0.00": {
-        slidesPerView: 1,
-        spaceBetwen: 10,
-      },
- 
-        "@0.75": {
-          slidesPerView: 1.5,
-          spaceBetwen: 10,
-        },
-     
-          "@0.85": {
-            slidesPerView: 1.5,
-            spaceBetwen: 20,
-          },
-    
-            "@1.00": {
-              slidesPerView: 2,
-              spaceBetwen: 40,
-            },
-    
-              "@1.40": {
-                slidesPerView: 2.25,
-                spaceBetwen: 40,
-              },
-     
-                "@1.50": {
-                  slidesPerView: 3,
-                  spaceBetwen: 50,
-                },
+const swiper = new Swiper('.live-swiper', {
+	slidesPerView: 3,
+	spaceBetwen: 30,
+	slidesPerGroupSkip: 1,
+	grabCursor: true,
+	keyboard: {
+		enabled: true,
+	},
 
-    },
- 
-  });
+	navigation: {
+		nextEl: '.live-swiper .swiper-button-next',
+		prevEl: '.live-swiper .swiper-button-prev',
+	},
+	breakpoints: {
+		'@0.00': {
+			slidesPerView: 1,
+			spaceBetwen: 10,
+		},
 
+		'@0.75': {
+			slidesPerView: 1.5,
+			spaceBetwen: 10,
+		},
+
+		'@0.85': {
+			slidesPerView: 1.5,
+			spaceBetwen: 20,
+		},
+
+		'@1.00': {
+			slidesPerView: 2,
+			spaceBetwen: 40,
+		},
+
+		'@1.40': {
+			slidesPerView: 2.25,
+			spaceBetwen: 40,
+		},
+
+		'@1.50': {
+			slidesPerView: 3,
+			spaceBetwen: 50,
+		},
+	},
+});
+
+// filter
+
+function toggleFil() {
+	const toggleFilter = document.querySelector('.toggleFilter');
+	const filterOptions = document.querySelector('.filterOptions');
+	toggleFilter.classList.toggle('active');
+	filterOptions.classList.toggle('active');
+}
